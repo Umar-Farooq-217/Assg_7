9ce045ce410be0330ee9df0049190d9a9742e0af
Mubashar <muhammadmubashar596@gmail.com>
2023-04-04 17:50:46 -0700

    ok

---
+++ umar.js
@@ -0,0 +1,418 @@
+    //  Q NO:1
+    // Write a js program to find maximum between two numbers.
+
+    // var umar1 = "lower value 33";
+    // var umar2 = "maximum value 99";
+    // // var umar = prompt("Enter a number ");
+   
+
+    // if (umar < 33){
+    //     console.log("The value is lower");
+    // } 
+    // else {
+    //    console.log("The value is maximum");
+      
+    // }
+
+    // ...............................................
+
+    // 2 Question
+    // Write a js program to find maximum between three numbers.
+    
+    //   var aa1 = "This is first value 12 " ;
+    //   var aa2 = "This is second value 24 " ;
+    //   var aa3 = "This is third value 36 " ;
+    //  var aa = prompt("Enter a number");
+    //  if (aa < 12 ){
+    //     console.log("This is first value");
+    //  }
+    //  else if (aa < 24 ){
+    //     console.log("This is moderate value");
+    //  } 
+    //  else {
+    //    console.log( "This is maximum value");
+    //  }
+    
+    
+    // ...............................................
+
+    //    3 Question
+    // Write s js program to find a number is positive,negative or zero.
+
+    // var bb = prompt("Enter a number")
+    // if (bb == 0){
+    //     console.log("The value is zero");
+    // }
+    // else if (bb > 0){
+    //     console.log("This is positive value ");
+    // }else{
+    //     console.log("This is negetive value");
+    // }
+
+
+    // ...............................................
+
+    // 4 th Question
+    //Write a js program to check whether a number is divisible by 5 and 11 or not.
+
+    // var cc = prompt ("Enter a number")
+    // if (cc %5==0){
+    //     console.log("The number is divisible by 5");
+    // }
+    // else if (cc %11==0){ 
+    //     console.log("The number is divisible by 11");
+    // }
+    // else{
+    //     console.log("The is not divisible by 5 and 11");
+    // }
+
+
+    // ...............................................
+
+    //   5th question
+    // Write a js program to whether a number is even or odd.
+
+    // var dd = prompt("Enter a number");
+    // if (dd %2==0){
+    //     console.log("The number is even");
+    // }
+    // else {
+    //     console.log("The number is odd");
+    // }
+
+
+    // ...............................................
+
+      //  No:6 Question
+    // Write a js program to check whether a year is leap year or not.
+
+    // var a6 = prompt("Enter the number of days in year ")
+    // if (a6 == 365){
+    //   console.log("This is leap year");
+    // }
+    // else{
+    //   console.log("This is not leap year");
+    // }
+      
+
+    // ...............................................
+
+    //  No:7 Question
+    // Write a js program to check a number is alphabet or not.
+
+
+    // var ff = prompt("Enter alphabet or number");
+    // if (ff > 0){
+    //     console.log("This is a number");
+    // }
+    // else {
+    //     console.log("This is alphabet");
+    // }
+
+    // ...............................................
+
+
+      //  No:8 Question
+      // Write a js program to input any alphabet and check whether it is alphabet
+      // or consonant.
+
+      // (not solved)
+      // var gg =prompt("Enter a chracter");
+      // if (gg == a){
+      //   console.log("This is a vowel");
+      // }
+      // else if (gg == e){
+      //   console.log("This is a vowel");
+      // }
+      // else if (gg == i){
+      //   console.log("This is a vowel");
+      // }
+      // else if (gg == o){
+      //   console.log("This is a vowel");
+      // }
+      // else if (gg == u){
+      //   console.log("This is a vowel");
+      // }
+
+      // else{
+      //   console.log("This is a consonant");
+      // }
+
+      // ...............................................
+
+       // No:9 Question( solved)
+       // Write a js program to input any character and check whether it is alphabet,
+      //  digit or special character
+
+      //  var hh = prompt("Enter a number")
+      //   if (hh < 1 ){
+      //     console.log("This is a special number");
+      //    //when the value will less than 1 
+      //   }
+      //    else if (hh  %1==0){
+      //     console.log("This is a number");
+      //   }
+      //   else{
+      //     console.log("This is alphabet");
+      //   }
+
+
+      // ...............................................
+  
+      // No:10 Question (not solved)
+      // Write a js program to check a character whether it is uppercase 
+      // or lowercase.
+
+      // var ii = prompt ("Enter a number")
+      // if (ii %A==a){
+      //   console.log("This is a uppercase");
+
+      // }
+      // else if(ii %B==b){
+      //   console.log("This is a uppercase");
+      // }
+      // else {
+      //   console.log("This is a lowercase");
+      // }
+
+      // ...............................................
+
+      // No:11 Question (solved)
+      // Write a js program to input week number and print week day.
+      
+      // var a11 = prompt("Enter week number")
+      // if (a11 == 1){
+      //   console.log("Sunday");
+      // }
+      // else if (a11==2){
+      //   console.log("Monday");
+      // }
+      // else if (a11==3){
+      //   console.log("Tuesday");
+      // }
+      // else if (a11==4){
+      //   console.log("Wednesday");
+      // }
+      // else if (a11==5){
+      //   console.log("Thursday");
+      // }
+      // else if (a11==6){
+      //   console.log("Friday");
+      // }
+      // else{
+      //   console.log("Saturday");
+      // }
+
+
+      // ...............................................
+
+      // No:12 Question (solved)
+
+      // Write a js program to input number of month and print number of days in
+      // that month.
+
+      // var a13 = prompt("enter a month No.")
+
+      // if (a13 == 1){
+      //   console.log("January has 31 days");
+      // }
+      // else if (a13 == 2){
+      //   console.log("February has 28 days");
+      // }
+      // else if (a13 == 3){
+      //   console.log("March has 31 days");
+      // }
+      // else if (a13 == 4){
+      //   console.log("April has 30 days");
+      // }
+      // else if (a13 == 5){
+      //   console.log("May has 31 days");
+      // }
+      // else if (a13 == 6){
+      //   console.log("June has 30 days");
+      // }
+      // else if (a13 == 7){
+      //   console.log("July has 31 days");
+      // }
+      // else if (a13 == 8){
+      //   console.log("August has 31 days");
+      // }
+      // else if (a13 == 9){
+      //   console.log("September has 30 days");
+      // }
+      // else if (a13 == 10){
+      //   console.log("Octomber has 31 days");
+      // }
+      // else if (a13 == 11){
+      //   console.log("November has 30 days");
+      // }
+      // else {
+      //   console.log("December has 31 days");
+      // }
+
+
+
+
+      // ...............................................
+
+
+      // No:13 Question (solve)
+
+      // Write a js program to count total number of notes in given amount.
+
+      // var b13 =+ prompt("Enter the amount");
+    
+      // // console.log("g jnb"+ b13/10);
+      // if (b13 >50000 ){
+      //   console.log(b13/5000  +  ":Notes of 5000 rupees");
+      // }
+      // else if(b13 > 10000){
+      //   console.log(b13/1000 +":Notes of 1000 rupees");
+      // }
+      // else if(b13 > 5000){
+      //   console.log(b13/500 +":Notes of 500 rupees");
+      // }
+      // else if(b13 > 1000){
+      //   console.log(b13/100 +":Notes of 100 rupees");
+      // }
+      // else if(b13 > 500){
+      //   console.log(b13/50 +":Notes of 50 rupees");
+      // }
+      // else if(b13 > 0){
+      //   console.log(b13/10 +":Notes of 10 rupees");
+      // }
+      // else{
+      //   console.log( "Please give number which has zero at end ");
+      // }
+      
+     
+
+      // ...............................................
+
+      // No:14 Question (not solve)
+
+    //   var a14a = prompt("Enter three angles for tringle") 
+    //   var a14b = prompt("Enter three angles for tringle") 
+    //   var a14c = prompt("Enter three angles for tringle") 
+     
+    //  if (a14a < 45){
+    //   console.log(" 1st side of Triangle is valid");
+    //  }
+    //  else if (a14b < 90){
+    //   console.log(" 2nd side of Triangle is valid");
+    //  }
+    //  else if (a14c < 45){
+    //   console.log(" 3rd side of Triangle is valid");
+    //  }
+    //  else{
+    //   console.log("  Triangle is not valid");
+    //  }
+
+    // ...............................................
+
+    // No:18 Question ( solve)
+
+    // var a18 = prompt("Enter your amount of day")
+    // // if you can not get 1000 rupees you can not get profit
+    // if (a18 < 1000){
+    //   console.log("You are losed");
+    // }
+    // else {
+    //   console.log("You had got profit");
+    // }
+
+
+    // ...............................................
+
+// No:19 Question ( solve)
+
+// Write a js program to input marks of five subjects Physics,Chemistry,Math,
+// Computer and English.Calculate percentage and grade according to following:
+// Percentage >= 90% :Grade A
+// Percentage >= 80% :Grade B
+// Percentage >= 70% :Grade C
+// Percentage >= 60% :Grade D
+// Percentage >= 50% :Grade E
+// Percentage >= 40% :Grade F
+// Percentage < 40% :Grade G
+
+//     var physics = 120;
+//     var Chemistry  = 145;
+//     var Biology = 138;
+//     var Math =130;
+//     var Computer  = 140;
+// var total = physics+Chemistry+Biology+Math+Computer;
+// // console.log(total / 750 * 100+"%");
+//  if (total > 675 ){
+// console.log("Grade A Greater than 90%");
+//  }  
+//  else if (total > 600){
+//   console.log("Grade B  Greater than 80%");
+//  }
+//  else if (total > 525){
+//   console.log("Grade C  Greater than 70%");
+//  }
+//  else if (total > 450){
+//   console.log("Grade D  Greater than 60%");
+//  }
+//  else if (total > 375){
+//   console.log("Grade E  Greater than 50%");
+//  }
+//  else if (total > 300){
+//   console.log("Grade F  Greater than 40%");
+//  }
+// else{
+//   "Lower than 40% Grade g"
+// }
+// ...............................................
+
+
+// No:20 Question ( solve)
+// Write a js program to input basic salary of an employee and calculate and its
+// Gross salary according to :
+// Basic salary <= 10000 : HRA = 20% ;DA = 80%
+// Basic salary <= 20000 : HRA = 25% ;DA = 90%
+// Basic salary > 20000 : HRA = 30% ;DA = 95%
+
+
+
+
+
+
+
+
+// ...............................................
+ 
+// No:21 Question ( solve)
+// Write a js program to input electricity unit charges and calculate total 
+// electricity bill according to given condition :
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For units above 250 Rs. 1.50/unit 
+// An additional supercharge of 20% added to bill
+
+var a21 = prompt("Enter units of electricity")
+
+if (a21 < 50){
+  console.log(  "Rs :" +0.50 * a21  );
+}
+else if (a21 < 100){
+  console.log("Rs :"+ 0.75* a21);
+}
+else if (a21 < 200){
+  console.log("Rs :"+ 1.20 * a21);
+}
+
+else{
+  console.log("Rs :" + 1.50 * a21);
+}
+
+
+
+
+
+
+
+
+// ...............................................
